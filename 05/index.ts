898ef5e4114b5279ec52f0cb93299426ca9604ba
IgorAPSantos <igorppandre@hotmail.com>
2023-12-11 21:47:20 -0300

    melhorando  ex5

--- 05/index.ts
+++ 05/index.ts
@@ -1,17 +1,6 @@
 const soletrar = (palavra: string): string => {
-    const letras: string[] = palavra.split('')
-    let resposta: string = ''
-
-    for (let i = 0; i < letras.length; i++) {
-        const letra = letras[i]
-        if (i === palavra.length - 1) {
-            resposta += `${letra}`
-            break
-        }
-        resposta += `${letra}-`
-    }
-
-    return resposta
+    const letras: string = palavra.split('').join('-')
+    return letras
 }
 
 console.log(soletrar('computadores'));
